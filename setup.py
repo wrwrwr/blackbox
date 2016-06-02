4451f924ce59436f7cec241f2488501f6ea13044
wrwrwr <git@wr.waw.pl>
2016-06-02 11:37:42 +0200

    Added a setup.py, mostly to list the testing requirements.
    There are two reasons for using the  meta dict and guarding with
    __name__ == '__main__'. First, it avoids having to ignore setup.py
    with some linting tools. Second, it lets your custom scripts pull
    out parts of the meta-data in the most obvious way.

---
+++ setup.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+
+from setuptools import setup
+
+meta = {
+    'name': 'blackbox',
+    'version': '0.1.0',
+    'packages': ('blackbox',),
+    'install_requires': ('cython>=0.24', 'numpy>=1.11', 'scipy>=0.17'),
+    'tests_require': ('pytest', 'pytest-benchmark', 'pytest-flake8',
+                      'pytest-isort', 'pytest-readme',
+                      'flake8-print', 'flake8-todo', 'pep8-naming'),
+    'description': "Specialized toolkit for the BlackBox Competition.",
+    'long_description': open('README.rst').read(),
+    'author': "Wojciech Ruszczewski",
+    'author_email': "blackbox@wr.waw.pl",
+    'url': "http://github.org/wrwrwr/blackbox",
+    'license': "MIT",
+    'classifiers': (
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Science/Research",
+        "Topic :: Scientific/Engineering :: Artificial Intelligence",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.4"
+    )
+}
+
+if __name__ == '__main__':
+    setup(**meta)
