0287d4612d1cdab25ca1bee396fe680300bac160
wrwrwr <git@wr.waw.pl>
2016-06-03 02:43:22 +0200

    Invented a trick to make pycodestyle treat cimports as imports.

---
+++ conftest.py
@@ -0,0 +1,25 @@
+"""
+Monkeypatch pycodestyle, to let it process cimports as imports.
+"""
+
+
+def patch_checker(checker_class):
+    old_checker_init = checker_class.__init__
+
+    def checker_init(self, *args, **kwargs):
+        old_checker_init(self, *args, **kwargs)
+        self.lines = [l.replace('cimport', 'import') for l in self.lines]
+
+    checker_class.__init__ = checker_init
+
+try:
+    from pycodestyle import Checker
+    patch_checker(Checker)
+except ImportError:
+    pass
+
+try:
+    from pep8 import Checker
+    patch_checker(Checker)
+except ImportError:
+    pass
